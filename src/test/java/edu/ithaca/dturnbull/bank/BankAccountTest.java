2f64f6307a6279804d23f646fb6344ce606d852a
drweb <dwebb@ithaca.edu>
2026-01-30 10:21:26 -0500

    organized test cases

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -24,16 +24,18 @@ class BankAccountTest {
 
     @Test
     void isEmailValidTest(){
+        //Valid Cases:
         assertTrue(BankAccount.isEmailValid( "a@b.com"));   // valid email address -- normal case -- class: valid emails
-        assertFalse(BankAccount.isEmailValid(""));         // empty string -- border case -- class: missing @
         assertTrue(BankAccount.isEmailValid( "a@bb.cc"));  //valid -- minimal length domain -- border case -- class: valid emails
+
+        //Invalid Cases:
+        assertFalse(BankAccount.isEmailValid(""));         // empty string -- border case -- class: missing @
         assertFalse(BankAccount.isEmailValid("testemail.com")); //no @ sign -- border case -- class: missing @
-        assertFalse(BankAccount.isEmailValid("@hotmail.com")); //no prefix before @ sign -- border case -- class: missing prefix
+        assertFalse(BankAccount.isEmailValid("@hotmail.com")); //no prefix before @ sign -- border case -- class: invalid prefix
         assertFalse(BankAccount.isEmailValid("hotmail.com@jbob")); //domain and prefix are swapped -- border case -- class: invalid domain
-        assertFalse(BankAccount.isEmailValid("a$$a@gmail.com")); //invalid characters -- border case -- class: invalid characters
+        assertFalse(BankAccount.isEmailValid("a$$a@gmail.com")); //invalid characters -- border case -- class: special characters
         assertFalse(BankAccount.isEmailValid("jbob@jimbo.h")); //invalid domain -- border case -- class: invalid domain
-        // I would recommend adding a test for null input, multiple @ signs, and the case of no text between the @ and the .
-        // Although I get why the empty string test is present, it could actually be a redundant test if an email will always be invalid when it does not have an @ symbol.
+
     }
 
     @Test
